c90ad9f97a3ad60a8971e06189f4d855220c011d
Byron Ferguson <ferguson.bs@gmail.com>
2022-08-05 14:30:20 -0500

    initial commit

---
+++ client/vite.config.js
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/graphql': 'http://localhost:3001',
+    },
+  },
+});
